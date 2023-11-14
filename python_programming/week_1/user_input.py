148eec4fd3a4498bb6d50b59f1cfd997e2f7d4d6
Asive Bekezulu <79581892+BaeBischops@users.noreply.github.com>
2023-11-14 15:40:09 +0200

    Add files via upload

---
+++ python_programming/week_1/user_input.py
@@ -0,0 +1,9 @@
+'''
+    Intro To Python Assignment
+    author: Asive Bekezulu
+'''
+name = input('please insert your full name')
+age = input('please insert your current age')
+location = input('please insert your current location')
+
+print(f'Hello, {name}, this year you are {age} of age and currently resides at {location}')
