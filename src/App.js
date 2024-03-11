18f3520269cd007d44d7edda06cc3e313fae488a
BaeBischops <dr.bkzee@gmail.com>
2024-03-11 17:34:32 +0200

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-function App(props) {
+export default function App() {
     return (
-        <div></div>
+        <div>Hello World</div>
     );
 }
-
-export default App;
